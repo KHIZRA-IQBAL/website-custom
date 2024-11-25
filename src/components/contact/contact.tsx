f5028893ea8d9c75d2d60b25c7c286512c79ab41
KHIZRA-IQBAL <iqbalkhizra026@gmail.com>
2024-11-26 04:16:48 +0500

    apossmistake

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -4,7 +4,7 @@ import styles from "@/components/contact/contact.module.css"
 const Contact = () => {
   return (
     <div className={styles.contact}>
-      <div className={styles.leftside}><h1>Let&aapos;s Talk <br />AND <br />Connect With Us</h1></div>
+      <div className={styles.leftside}><h1>Let&apos;s Talk <br />AND <br />Connect With Us</h1></div>
       <div className={styles.rightside}>
         <p>Email: yourstyle@gmail.com</p>
         <p>Contact: 020035784666663</p>
